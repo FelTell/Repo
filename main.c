d9dca070dc30544e989f0a1b66379214db254a52
Felipe Telles <felipe.melo.telles@gmail.com>
2020-01-29 18:27:38 -0300

    erros

--- main.c
+++ main.c
@@ -8,11 +8,11 @@ int media(int num1, int num2){
 
 int main(){
     int num1, num2, aux
-    printf("Dyjite dois numeros separados por espaco:\n");
-    scanf("%d %d", &num1, num2);
+    printf("Digite dois numeros separados por espaco:\n");
+    scanf("%d %d", &num1, &num2);
 
     aux = media(num1, num2);
-    printf("A media dos dois numeros eh: %d",aux)
+    printf("A media dos dois numeros eh: %d\n",aux)
 
     return 0;
 }
